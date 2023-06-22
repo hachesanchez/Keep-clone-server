154b80765218db5fed527e24af21748804bdb77a
Helena Sánchez <helena.sangar@gmail.com>
2023-06-22 21:12:08 +0200

    env and model

---
+++ models/Note.model.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose')
+
+const noteSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'El título de la nota es obligario']
+        },
+        text: {
+            type: String,
+            required: [true, 'Añadir texto es obligatorio']
+        },
+        image: {
+            type: String
+        },
+        datePosted: {
+            type: Date,
+            default: Date.now
+        },
+        dateModified: {
+            type: Date,
+            default: Date.now
+        },
+        reminder: {
+            date: {
+                type: Date,
+                default: null
+            },
+            location: {
+                type: String,
+                default: null
+            }
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        collaborators: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        tag: {
+            type: Schema.Types.ObjectId,
+            ref: "Tag"
+        }
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Note = model('Note', noteSchema)
+
+module.exports = Note
